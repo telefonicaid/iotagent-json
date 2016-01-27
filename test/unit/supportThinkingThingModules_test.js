85757eef78c1acea17a80172514102fbc074cb38
Daniel Moran <dmj@tid.es>
2016-01-27 14:58:37 +0100

    FIX P1 and C1 TT Modules not working in single attribute mode

--- test/unit/supportThinkingThingModules_test.js
+++ test/unit/supportThinkingThingModules_test.js
@@ -69,7 +69,7 @@ describe('Support for Thinking Things Modules', function() {
         iotagentMqtt.stop(done);
     });
 
-    describe('When a new measure with Thinking Thing module P1 arrives', function() {
+    describe('When a new measure with Thinking Thing module P1 arrives to a multiattribute topic', function() {
         beforeEach(function() {
             contextBrokerMock
                 .matchHeader('fiware-service', 'smartGondor')
@@ -84,6 +84,26 @@ describe('Support for Thinking Things Modules', function() {
             };
 
             mqttClient.publish('/1234/MQTT_2/attributes', JSON.stringify(values), null, function(error) {
+                setTimeout(function() {
+                    contextBrokerMock.done();
+                    done();
+                }, 100);
+            });
+        });
+    });
+
+    describe('When a new measure with Thinking Thing module P1 arrives to a single attribute topic', function() {
+        beforeEach(function() {
+            contextBrokerMock
+                .matchHeader('fiware-service', 'smartGondor')
+                .matchHeader('fiware-servicepath', '/gardens')
+                .post('/v1/updateContext', utils.readExampleFile('./test/contextRequests/TTModuleP1Single.json'))
+                .reply(200, utils.readExampleFile('./test/contextResponses/TTModuleP1SingleSuccess.json'));
+        });
+        it('should send its value to the Context Broker', function(done) {
+            var values = '214,7,d22,b00,-64,';
+
+            mqttClient.publish('/1234/MQTT_2/attributes/P1', values, null, function(error) {
                 setTimeout(function() {
                     contextBrokerMock.done();
                     done();
