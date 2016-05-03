6f52034b035a3f239117520b70d178a75cf2d04a
Daniel Moran <dmj@tid.es>
2016-05-03 17:15:01 +0200

    Update command-line interface

--- bin/iotaMqttTester.js
+++ bin/iotaMqttTester.js
@@ -28,7 +28,7 @@
 var fs = require('fs'),
     defaultConfig = require('../client-config.js'),
     commandLine = require('iotagent-node-lib').commandLine,
-    clUtils = require('command-node'),
+    clUtils = commandLine.clUtils,
     mqtt = require('mqtt'),
     async = require('async'),
     _ = require('underscore'),
@@ -171,4 +171,4 @@ var commands = {
 commands = _.extend(commands, commandLine.commands);
 commandLine.init(configCb, configIot);
 
-clUtils.initialize(commands, 'IoT Agent tester> ');
+clUtils.initialize(commands, 'MQTT Agent tester> ');
