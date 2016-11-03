f10ec1f542fc623fcc902b82fb8d474ae1ce26fa
Daniel Moran <dmj@tid.es>
2016-11-03 15:55:36 +0100

    FIX Timestamp parameter ignored

--- test/unit/HTTP_reveice_measures-test.js
+++ test/unit/HTTP_reveice_measures-test.js
@@ -205,6 +205,72 @@ describe('HTTP: Measure reception ', function() {
         });
     });
 
+    describe('When a POST measure arrives with a TimeInstant query parameter in the body', function() {
+        var optionsMeasure = {
+                url: 'http://localhost:' + config.http.port + '/iot/d',
+                method: 'POST',
+                json: {
+                    humidity: '111222'
+                },
+                headers: {
+                    'fiware-service': 'smartGondor',
+                    'fiware-servicepath': '/gardens'
+                },
+                qs: {
+                    i: 'dev0130101',
+                    k: '1234',
+                    t: '2020-02-22T22:22:22'
+                }
+            },
+            provisionOptions = {
+                url: 'http://localhost:' + config.iota.server.port + '/iot/devices',
+                method: 'POST',
+                json: utils.readExampleFile('./test/deviceProvisioning/provisionDeviceTimeinstant.json'),
+                headers: {
+                    'fiware-service': 'smartGondor',
+                    'fiware-servicepath': '/gardens'
+                }
+            };
+
+        beforeEach(function(done) {
+            nock.cleanAll();
+
+            contextBrokerMock = nock('http://192.168.1.1:1026')
+                .matchHeader('fiware-service', 'smartGondor')
+                .matchHeader('fiware-servicepath', '/gardens')
+                .post('/v1/updateContext')
+                .reply(200, utils.readExampleFile('./test/contextResponses/multipleMeasuresSuccess.json'));
+
+            contextBrokerMock
+                .matchHeader('fiware-service', 'smartGondor')
+                .matchHeader('fiware-servicepath', '/gardens')
+                .post('/v1/updateContext', utils.readExampleFile('./test/contextRequests/timeInstantMeasures.json'))
+                .reply(200, utils.readExampleFile('./test/contextResponses/timeInstantMeasuresSuccess.json'));
+
+            iotagentMqtt.stop(function() {
+                config.iota.timestamp = true;
+                config.compressTimestamp = false;
+                iotagentMqtt.start(config, function() {
+                    request(provisionOptions, function(error, response, body) {
+                        done();
+                    });
+                });
+            });
+        });
+
+        afterEach(function() {
+            config.iota.timestamp = false;
+            config.compressTimestamp = true;
+        });
+
+        it('should send its value to the Context Broker', function(done) {
+            request(optionsMeasure, function(error, result, body) {
+                contextBrokerMock.done();
+                done();
+            });
+        });
+    });
+
     describe('When a POST measure arrives for an unprovisioned device', function() {
         var optionsMeasure = {
             url: 'http://localhost:' + config.http.port + '/iot/d',
