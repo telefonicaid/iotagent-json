10e972baa4218dae81ac2c842726948beb6ce92b
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2018-06-06 14:49:59 +0200

    enable qos and retain in mqtt

--- config.js
+++ config.js
@@ -25,7 +25,9 @@ var config = {};
 config.mqtt = {
     host: 'localhost',
     port: 1883,
-    thinkingThingsPlugin: true
+    thinkingThingsPlugin: true,
+    qos: 2,
+    retain: true
 };
 
 config.iota = {
