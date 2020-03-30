4c864db1869b5bdddd9bf0ef0d8c9b25499b5540
Jason Fox <jason-fox@users.noreply.github.com>
2020-03-30 12:32:14 +0200

    Add Mongo-DB User and Password as Docker Secrets
    Once Mongo-DB  User and Password are fully supported within the iot-node-lib, they should be made available obfuscated within Docker 
    
    Related: https://github.com/telefonicaid/iotagent-node-lib/pull/852

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -47,6 +47,8 @@ file_env 'IOTA_AUTH_USER'
 file_env 'IOTA_AUTH_PASSWORD'
 file_env 'IOTA_AUTH_CLIENT_ID'
 file_env 'IOTA_AUTH_CLIENT_SECRET'
+file_env 'IOTA_MONGO_USER'
+file_env 'IOTA_MONGO_PASSWORD'
 
 if [[  -z "$IOTA_AUTH_ENABLED" ]]; then
  echo "***********************************************"
