df397c40dac487af1a3aa31ebb4704b2a087e4fd
jmezzera <43732985+jmezzera@users.noreply.github.com>
2020-06-30 14:04:24 -0300

    Update config.js
    Co-authored-by: Fermín Galán Márquez <fgalan@users.noreply.github.com>

--- config.js
+++ config.js
@@ -104,7 +104,7 @@ config.mqtt = {
     /**
      * Whether to use slashes at the beginning of topic when sending or not
      */
-    avoidLeadingSlashes: false
+    avoidLeadingSlash: false
 };
 
 /**
