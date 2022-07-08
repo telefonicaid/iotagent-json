830663480f04425b94be619649125ef7e54058d0
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2022-07-08 11:08:09 +0200

    add test

--- test/unit/ngsiv2/HTTP_commands_test.js
+++ test/unit/ngsiv2/HTTP_commands_test.js
@@ -108,7 +108,108 @@ describe('HTTP: Commands', function () {
 
             mockedClientServer = nock('http://localhost:9876')
                 .post('/command', function (body) {
-                    return body.PING || body.PING.data || body.PING.data === 22;
+                    return body || body === 'MQTT_2AnMQTTDevice';
+                })
+                .reply(200, '{"PING":{"data":"22"}}');
+        });
+
+        it('should return a 204 OK without errors', function (done) {
+            request(commandOptions, function (error, response, body) {
+                should.not.exist(error);
+                response.statusCode.should.equal(204);
+                done();
+            });
+        });
+        it('should update the status in the Context Broker', function (done) {
+            request(commandOptions, function (error, response, body) {
+                contextBrokerMock.done();
+                done();
+            });
+        });
+        it('should publish the command information in the MQTT topic', function (done) {
+            request(commandOptions, function (error, response, body) {
+                setTimeout(function () {
+                    mockedClientServer.done();
+                    done();
+                }, 100);
+            });
+        });
+    });
+});
+
+describe('HTTP: Commands with expressions', function () {
+    beforeEach(function (done) {
+        const provisionOptions = {
+            url: 'http://localhost:' + config.iota.server.port + '/iot/devices',
+            method: 'POST',
+            json: utils.readExampleFile('./test/deviceProvisioning/provisionCommandHTTP2.json'),
+            headers: {
+                'fiware-service': 'smartgondor',
+                'fiware-servicepath': '/gardens'
+            }
+        };
+
+        config.logLevel = 'INFO';
+
+        nock.cleanAll();
+
+        contextBrokerMock = nock('http://192.168.1.1:1026')
+            .matchHeader('fiware-service', 'smartgondor')
+            .matchHeader('fiware-servicepath', '/gardens')
+            .post('/v2/registrations')
+            .reply(201, null, { Location: '/v2/registrations/6319a7f5254b05844116584d' });
+
+        contextBrokerMock
+            .matchHeader('fiware-service', 'smartgondor')
+            .matchHeader('fiware-servicepath', '/gardens')
+            .post('/v2/entities?options=upsert')
+            .reply(204);
+
+        iotagentMqtt.start(config, function () {
+            request(provisionOptions, function (error, response, body) {
+                done();
+            });
+        });
+    });
+
+    afterEach(function (done) {
+        nock.cleanAll();
+        async.series([iotAgentLib.clearAll, iotagentMqtt.stop], done);
+    });
+
+    describe('When a command arrive to the Agent for a device with the HTTP protocol', function () {
+        const commandOptions = {
+            url: 'http://localhost:' + config.iota.server.port + '/v2/op/update',
+            method: 'POST',
+            json: utils.readExampleFile('./test/unit/ngsiv2/contextRequests/updateCommand1.json'),
+            headers: {
+                'fiware-service': 'smartgondor',
+                'fiware-servicepath': '/gardens'
+            }
+        };
+
+        beforeEach(function () {
+            contextBrokerMock
+                .matchHeader('fiware-service', 'smartgondor')
+                .matchHeader('fiware-servicepath', '/gardens')
+                .patch(
+                    '/v2/entities/Second%20MQTT%20Device/attrs?type=AnMQTTDevice',
+                    utils.readExampleFile('./test/unit/ngsiv2/contextRequests/updateStatus1.json')
+                )
+                .reply(204);
+
+            contextBrokerMock
+                .matchHeader('fiware-service', 'smartgondor')
+                .matchHeader('fiware-servicepath', '/gardens')
+                .patch(
+                    '/v2/entities/Second%20MQTT%20Device/attrs?type=AnMQTTDevice',
+                    utils.readExampleFile('./test/unit/ngsiv2/contextRequests/updateStatus6.json')
+                )
+                .reply(204);
+
+            mockedClientServer = nock('http://localhost:9876')
+                .post('/command', function (body) {
+                    return body || body === 'MQTT_2AnMQTTDevice';
                 })
                 .reply(200, '{"PING":{"data":"22"}}');
         });
