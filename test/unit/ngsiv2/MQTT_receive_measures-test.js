77ec087935d6872e361bb9f3a73aef85e99d6617
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2025-01-30 16:44:24 +0100

    add test

--- test/unit/ngsiv2/MQTT_receive_measures-test.js
+++ test/unit/ngsiv2/MQTT_receive_measures-test.js
@@ -362,6 +362,36 @@ describe('MQTT: Measure reception ', function () {
         });
     });
 
+    describe('When a new single array measure of 1 element arrives to the MQTT Topic', function () {
+        const values = [{ timestamp: '2025-01-27T09:00:00Z', value: 10.67 }];
+        beforeEach(function () {
+            contextBrokerMock
+                .matchHeader('fiware-service', 'smartgondor')
+                .matchHeader('fiware-servicepath', '/gardens')
+                .post(
+                    '/v2/entities?options=upsert',
+                    utils.readExampleFile('./test/unit/ngsiv2/contextRequests/singleArrayMeasure2.json')
+                )
+                .reply(204);
+        });
+        it('should send its values to the Context Broker', function (done) {
+            mqttClient.publish('/1234/MQTT_2/attrs/temperature', JSON.stringify(values), null, function (error) {
+                setTimeout(function () {
+                    contextBrokerMock.done();
+                    done();
+                }, 100);
+            });
+        });
+        it('should send its values to the Context Broker (without leading slash)', function (done) {
+            mqttClient.publish('json/1234/MQTT_2/attrs/temperature', JSON.stringify(values), null, function (error) {
+                setTimeout(function () {
+                    contextBrokerMock.done();
+                    done();
+                }, 100);
+            });
+        });
+    });
+
     describe('When a malformed multiple measure arrives to the MQTT Topic', function () {
         it('should not crash', function (done) {
             mqttClient.publish('/1234/MQTT_2/attrs', '{"humidity": " }(}', null, function (error) {
